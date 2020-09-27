e28e037eba1061bf85e5fe4ac9cbaa293f5428b2
Anton Akhmerov <anton.akhmerov@gmail.com>
2020-09-27 17:20:15 +0200

    wip

--- code/render_talks.py
+++ code/render_talks.py
@@ -6,12 +6,19 @@ import pytz
 from ruamel.yaml import YAML
 
 yaml = YAML(typ='safe')
-with open('../speakers_corner_talks.yml') as f:
+with open('../talks.yml') as f:
     talks = yaml.load(f)
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader('../templates')
+)
 
 for talk in talks:
     talk['time'] = talk['time'].replace(tzinfo=pytz.UTC)
 
-template = jinja2.Template(Path('speakers_corner.md.j2').read_text())
-
-Path('../speakers_corner.md').write_text(template.render(talks=talks, now=datetime.now(tz=pytz.UTC)))
+Path('../speakers_corner.md').write_text(
+    env.get_template('speakers_corner.md.j2').render(
+        talks=[talk for talk in talks if talk['event_type'] == 'speakers_corner'],
+        now=datetime.now(tz=pytz.UTC)
+    )
+)
