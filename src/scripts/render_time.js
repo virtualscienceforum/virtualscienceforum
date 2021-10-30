dad0438d1522a1da087d05fc9b6bff02f64b2d7c
Anton Akhmerov <anton.akhmerov@gmail.com>
2021-10-30 16:46:51 +0200

    remove underscores from timezones

--- src/scripts/render_time.js
+++ src/scripts/render_time.js
@@ -3,7 +3,7 @@ dayjs.extend(window.dayjs_plugin_utc)
 document$.subscribe(function() {
     var timezone = document.getElementById("timezone")
     if (!!timezone) {
-        timezone.innerText = Intl.DateTimeFormat().resolvedOptions().timeZone
+        timezone.innerText = Intl.DateTimeFormat().resolvedOptions().timeZone.replace("_", " ")
     };
     for (let time of document.getElementsByTagName("time")) {
         let format = time.getAttribute("data-format");
